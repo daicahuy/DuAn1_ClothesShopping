f6b3f22c702e5b6ed0ee97b6448867eef31b0b1d
manhnd46076 <162654101+manhnd46076@users.noreply.github.com>
2024-03-26 14:15:39 +0700

    CRUD danhmuc (#3)

--- app/views/admin/danhmuc/danhmucan.php
+++ app/views/admin/danhmuc/danhmucan.php
@@ -5,59 +5,43 @@
     <?php } ?>
     <!--  -->
     <div class="card-body">
-        <h5 class="card-title">Bảng danh mục</h5>
-        <a href="?url=admin/danhmuc/danhmucan">
-                                <button style="margin-left: 12px;" type="submit" class="btn btn-success">
-                                    Danh mục đã ẩn
-                                </button>
-                            </a>
+        <h5 class="card-title">Bảng danh mục ẩn</h5>
         <div class="table-responsive">
             <table id="zero_config" class="table table-striped table-bordered">
-                
                 <thead>
                     <tr>
                         <th>Tên danh mục</th>
                         <th>Tên loại</th>
                         <th>
                             Hành động
-                            <a href="?url=admin/danhmuc/them">
-                                <button style="margin-left: 12px;" type="submit" class="btn btn-success">
-                                    Thêm
-                                </button>
-                            </a>
+
                         </th>
                     </tr>
                 </thead>
-                
                 <tbody>
-                    <?php foreach ($listDanhMuc as $key => $danhMuc) : ?>
+                    <?php foreach ($listDanhMucAn as $key => $danhMuc) : ?>
 
 
 
                         <tr>
-
                             <td><?= $danhMuc['ten_danh_muc'] ?></td>
-                            <?php $danhMucTens = getDanhMucTen($danhMuc['ten_danh_muc'])   ?>
+                            <?php $danhMucTenAn = getDanhMucTenAn($danhMuc['ten_danh_muc'])   ?>
                             <td>
-                                <?php foreach ($danhMucTens as $key => $danhMucTen) : ?>
+                                <?php foreach ($danhMucTenAn as $key => $danhMucTen) : ?>
                                     <?php echo $danhMucTen['ten_loai'] ?>
                                 <?php endforeach ?>
                             </td>
-
                             <td>
-                                <a href="?url=admin/danhmuc/sua&ten_danh_muc=<?= $danhMuc['ten_danh_muc'] ?>" class="btn btn-warning">
-                                    Sửa
-                                </a>
+
 
                                 <!-- <a href="?url=admin/danhmuc/xoa&ten_danh_muc=<?= $danhMuc['ten_danh_muc'] ?>" class="btn btn-danger">
                                     Xóa
                                 </a> -->
-                                <a href="?url=admin/danhmuc/an&ten_danh_muc=<?= $danhMuc['ten_danh_muc'] ?>" class="btn btn-danger">
-                                    Ẩn
+                                <a href="?url=admin/danhmuc/hien&ten_danh_muc=<?= $danhMuc['ten_danh_muc'] ?>" class="btn btn-danger">
+                                    Hiện
                                 </a>
                             </td>
                         </tr>
-
 
                     <?php endforeach ?>
                 <tbody>
